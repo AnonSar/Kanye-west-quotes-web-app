20724984cbb920abc53bf35253459a28710a49c4
ANMOL SARRAF <asar0011@student.monash.edu>
2020-03-05 17:10:40 +1100

    Everything has been fixed except for the background video.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
-import HeadingSection from "./Components/Heading-Section/heading-section";
 import BackgroundVideo from "./Components/Background-video/background-video";
 
 class App extends Component {
   render() {
-    return (
-      <div className="main-container">
-        {/* <BackgroundVideo /> */}
-        <HeadingSection />
-      </div>
-    );
+    return <BackgroundVideo />;
   }
 }
 
--- src/Components/Background-video/background-video.js
+++ src/Components/Background-video/background-video.js
@@ -1,10 +1,14 @@
 import React from "react";
+import HeadingSection from "../Heading-Section/heading-section";
 
 const BackgroundVideo = Props => {
   return (
-    <video autoPlay muted loop className="backgroundVideo">
-      <source src="./backgroundVideo.mp4" type="video/mp4" />
-    </video>
+    <div>
+      <video autoplay muted loop className="backgroundVideo">
+        <source src="./backgroundVideo.mp4" type="video/mp4" />
+      </video>
+      <HeadingSection />
+    </div>
   );
 };
 
