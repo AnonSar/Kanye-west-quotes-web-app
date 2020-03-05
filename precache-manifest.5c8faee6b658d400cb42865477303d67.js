dce1e4fbd62171ad9d52e861e09b099ae750242c
ANMOL SARRAF <asar0011@student.monash.edu>
2020-03-06 09:42:37 +1100

    Updates

---
+++ precache-manifest.5c8faee6b658d400cb42865477303d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f142a4a3302b1ed66bae42e1c5d4bbdf",
+    "url": "/Kanye-west-quotes-web-app/index.html"
+  },
+  {
+    "revision": "e1b4da97c5ae2a76f7e8",
+    "url": "/Kanye-west-quotes-web-app/static/css/main.9523578c.chunk.css"
+  },
+  {
+    "revision": "2e6dc87893eba1efa730",
+    "url": "/Kanye-west-quotes-web-app/static/js/2.9352fa46.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Kanye-west-quotes-web-app/static/js/2.9352fa46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b4da97c5ae2a76f7e8",
+    "url": "/Kanye-west-quotes-web-app/static/js/main.dccbf234.chunk.js"
+  },
+  {
+    "revision": "839ce92827ca61ee33a9",
+    "url": "/Kanye-west-quotes-web-app/static/js/runtime-main.2b4dd447.js"
+  },
+  {
+    "revision": "4f878a42446b31d23a159ec79a8727af",
+    "url": "/Kanye-west-quotes-web-app/static/media/backgroundVideo.4f878a42.mp4"
+  }
+]);
