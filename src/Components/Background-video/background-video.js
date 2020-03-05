839b6c74edbe2aafdccf75becf02dfc48f0815cc
ANMOL SARRAF <asar0011@student.monash.edu>
2020-03-05 17:27:19 +1100

    Background video is working

--- src/Components/Background-video/background-video.js
+++ src/Components/Background-video/background-video.js
@@ -1,11 +1,12 @@
 import React from "react";
+import video from "./backgroundVideo.mp4";
 import HeadingSection from "../Heading-Section/heading-section";
 
 const BackgroundVideo = Props => {
   return (
     <div>
-      <video autoplay muted loop className="backgroundVideo">
-        <source src="./backgroundVideo.mp4" type="video/mp4" />
+      <video autoPlay muted loop className="backgroundVideo">
+        <source src = {video} type="video/mp4" />
       </video>
       <HeadingSection />
     </div>
