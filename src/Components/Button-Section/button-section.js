15a4089e662f7cb06c0fafe0f231af7facbaa427
ANMOL SARRAF <asar0011@student.monash.edu>
2020-04-20 18:00:42 +1000

    Chagne the styling of the button component

--- src/Components/Button-Section/button-section.js
+++ src/Components/Button-Section/button-section.js
@@ -4,10 +4,16 @@ class ButtonSection extends Component {
   render() {
     return (
       <div className="button-section">
-        <button className="quote-button" onClick = {this.props.handleButtonClick}>GET-ME-A-QOUTE</button>
+        <button
+          style={{ cursor: "pointer" }}
+          className="quote-button"
+          onClick={this.props.handleButtonClick}
+        >
+          GET-ME-A-QOUTE
+        </button>
       </div>
     );
   }
-};
+}
 
 export default ButtonSection;
